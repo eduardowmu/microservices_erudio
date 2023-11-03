373041631eeca42f3c12a9b0434f3085dfa53c09
eduardo.murakoshi@groove.tech <eduardo.murakoshi@groove.tech>
2023-11-03 17:56:35 -0300

    config server basico

---
+++ config.server/src/main/java/br/edu/app/config/server/Application.java
@@ -0,0 +1,13 @@
+package br.edu.app.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
