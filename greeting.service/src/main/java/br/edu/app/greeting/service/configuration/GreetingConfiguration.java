5ca3f54e0a10e8ef77daddba3882fda6989f7d82
eduardo.murakoshi@groove.tech <eduardo.murakoshi@groove.tech>
2023-11-07 22:59:59 -0300

    alteração de properties externa sem interromper o microserviço

--- greeting.service/src/main/java/br/edu/app/greeting/service/configuration/GreetingConfiguration.java
+++ greeting.service/src/main/java/br/edu/app/greeting/service/configuration/GreetingConfiguration.java
@@ -4,11 +4,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 @NoArgsConstructor
 @Getter
 @Setter
 @Component
+@RefreshScope//Essa notação nos permite fazer alteração nas propriedades sem precisar interromper a execução
 @ConfigurationProperties("greeting-service")
 public class GreetingConfiguration {
     private String greeting;
